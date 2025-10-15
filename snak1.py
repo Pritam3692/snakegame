8e9cfd0ff870b9066b96972e829355d881bab815
Pritam3692 <pp3692@srmist.edu.in>
2025-10-15 23:57:43 +0530

    Add files via upload

---
+++ snak1.py
@@ -0,0 +1,9 @@
+
+import matplotlib.pyplot as plt
+GROUP=['VII','VIII','IX','X']
+NO_STUDENTS=[40,45,35,45]
+plt.bar(GROUP,NO_STUDENTS)
+plt.xlabel("CLASS")
+plt.ylabel("STUDENTS")
+plt.title("NUMBERS OF STUDENTS IN CLASS")
+plt.show()
